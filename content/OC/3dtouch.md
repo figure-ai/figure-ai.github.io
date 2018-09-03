ba7edc6b4184c0c8a7c75638fdd6e3f5747c60f5
Answer_mac <592134873@qq.com>
2018-09-03 13:35:42 +0800

    Update output documentation

--- content/OC/3dtouch.md
+++
@@ -1,8 +0,0 @@
----
-title: OC-3DTouch
-layout: page
-date: 2018-05-14 00:00
----
-
-
-
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-echo -e "-------------simiki g-------------"
-simiki g
-echo -e "-------------simiki depoly------------"
-ghp-import -p -m "Update output documentation" -r origin -b master output
